03272ba9662328fc3fd7526a8115d179e0f743c9
Ulises <leobardo.ulises309@gmail.com>
2017-05-10 20:06:11 -0600

    Añadiendo registrar a la navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -12,7 +12,7 @@
     <div id="navbar-options" class="collapse navbar-collapse">
       <ul class="nav navbar-nav navbar-right">
         <li><a href="{{url('/')}}">Inicio</a></li>
-        <li><a href="{{url('/contacto')}}">Contacto</a></li>
+        <li><a href="{{url('/registrar')}}">Registrar</a></li>
         <li><a href="{{url('/iniciarSesion')}}">Iniciar Sesi&oacute;n</a></li>
       </ul>
     </div>
